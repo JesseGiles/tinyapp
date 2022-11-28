230e0e2792aae4d7b07a3c6a1f958ac96b95d12f
Jesse Giles <jesse.giles@telus.com>
2022-11-28 16:29:32 -0700

    created urls_new with form submission functionality, added app.use(express.urlencoded) function to server to take form submission and return object containing longURL value

--- express_server.js
+++ express_server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //set embedded js as template viewer
 
 const urlDatabase = { //object storing data for templates
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {}
+
+app.use(express.urlencoded({ extended: true })); 
+//this is a built-in middleware function in exprses. parses incoming requests created by a form submission (urls_new) so you can access data submitted using the req body (converts url encoded data to strings, otherwise body may show as undefined)
+//So using our urls_new form as an example, the data in the input field will be avaialbe to us in the req.body.longURL variable, which we can store in our urlDatabase object. 
+
+app.post("/urls", (req, res) => { //this function actions when form submitted
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => { //get "/" is main url, displays hello msg
   res.send("Hello!");
@@ -17,6 +28,10 @@ app.get("/urls", (req, res) => { //adds "/urls" route to main url
   const templateVars = { urls: urlDatabase }; //give key to obj for use in urls_index
   res.render("urls_index", templateVars); //render html found on urls_index.ejs file, pass along templateVars object for use in that file
 });
+
+app.get("/urls/new", (req, res) => { //adds "urls/new" route
+  res.render("urls_new"); //utlizing urls_new.js
+})
 
 app.get("/urls/:id", (req, res) => { //adds "urls/(x)"" x param can be any value entered at url but we are storing specifics in urlDatabase
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }; //obj storing the entered url param(anything after ":" and associated longURL if param matches databse)
